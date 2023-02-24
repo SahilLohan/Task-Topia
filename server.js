e4b24e55357028cb17b26e0acbaeb6f27835d022
Sahil <sahillohan07@gmail.com>
2023-02-24 19:46:04 +0530

    Whole UI and working without Database is complete

--- server.js
+++ server.js
@@ -4,21 +4,22 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
+var array=[];
 app.get("/",function(req,res){
     var today = new Date();
     var week = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday');
     var dayjs  = week[today.getDay()];
     res.render('list.ejs',{
-        day:dayjs
+        day:dayjs,
+        items:array
     })
 
 })
 
 app.post("/",function(req,res){
-    
         console.log(req.body.Task);
-        res.send("Banana ki jgh loda lele bsdk");
-    
+        array.push(req.body.Task);
+        res.redirect("/");
 })
 app.listen(3000,()=>{
     console.log("Server started on Port : 3000 ");
